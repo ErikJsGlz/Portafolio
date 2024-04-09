15463a37c436517cd3828033ee42d303165ede46
Erik Gonzalez <erik.gonzalez@christuscei.mx>
2024-04-09 11:20:49 -0600

    Final Setup

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import "./headerStyles.css";
+
+export default function Header() {
+    return (
+        <div id="header">
+            <h4 className="span-hover span-hover--active">SOBRE MÍ</h4>
+            <h4 className="span-hover">PROYECTOS</h4>
+            <h4 className="span-hover">EXPERIENCIA</h4>
+        </div>
+    );
+}
