a20d66a9dd99bbb0ec026ced52b0ddc786d84060
ErikJsGlz <erikjs.glz@gmail.com>
2026-01-08 21:50:54 -0600

    Feat: Added Images and logic for changed project

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,20 @@
-import { Box, duration, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { AnimatePresence, motion } from 'framer-motion';
 
+import { useHomeContext } from './Provider/useHomeContext';
+import { HomeProvider } from './Provider/HomeProvider';
 import Header from '../Components/Header/Header';
 import { Orbs } from '../Components/Orb/Orbs';
-
 import './Home.scss';
-import { HomeProvider } from './Provider/HomeProvider';
-import { useHomeContext } from './Provider/useHomeContext';
 
 function Component() {
-	const { currentProjectIndex, currentTitle } = useHomeContext();
+	const {
+		currentProjectIndex,
+		currentTitle,
+		currentInformationLabel,
+		currentImage,
+		onClickImage,
+	} = useHomeContext();
 
 	return (
 		<Box className="home">
@@ -18,21 +23,76 @@ function Component() {
 
 			<AnimatePresence mode="wait">
 				<motion.div
+					key={currentProjectIndex}
+					className="home__body"
+					style={{
+						display: currentProjectIndex !== 0 ? 'flex' : 'none',
+					}}
+				>
+					<Typography>{currentInformationLabel}</Typography>
+				</motion.div>
+			</AnimatePresence>
+
+			<AnimatePresence mode="wait">
+				<motion.div
 					key={currentTitle}
 					className="home__title"
 					initial={{
+						translateX: -100,
+						opacity: 0,
+					}}
+					animate={{
 						translateX: 0,
+						opacity: 1,
+					}}
+					exit={{
+						translateX: 100,
+						opacity: 0,
 					}}
 					transition={{
-						translate: {
-							duration: 0.5,
-							ease: [0.4, 0, 0.2, 1],
-						},
+						duration: 0.5,
+						ease: [0.4, 0, 0.2, 1],
 					}}
 				>
 					{currentTitle}
 				</motion.div>
 			</AnimatePresence>
+
+			<Box
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+				className="home__image-container"
+			>
+				<AnimatePresence mode="popLayout">
+					{currentImage && (
+						<motion.img
+							key={currentImage}
+							src={currentImage}
+							onClick={onClickImage}
+							whileHover={{ scale: 1.2 }}
+							className="home__image"
+							initial={{
+								translateX: -100,
+								opacity: 0,
+							}}
+							animate={{
+								translateX: 0,
+								opacity: 1,
+							}}
+							exit={{
+								translateX: 100,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 0.3,
+								ease: [0.4, 0, 0.2, 1],
+							}}
+						/>
+					)}
+				</AnimatePresence>
+			</Box>
+
 			<Box className="background" />
 		</Box>
 	);
--- src/Pages/Provider/HomeProvider.tsx
+++ src/Pages/Provider/HomeProvider.tsx
@@ -1,26 +1,76 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 import { HomeContext } from './HomeContext';
+
+import MyWund01 from '../../assets/img/mywund_layout/mywund_mobile_01.png';
+import MyWund02 from '../../assets/img/mywund_layout/mywund_mobile_02.png';
+import MyWund03 from '../../assets/img/mywund_layout/mywund_mobile_03.png';
+import MyWund04 from '../../assets/img/mywund_layout/mywund_mobile_04.png';
+import MyWund05 from '../../assets/img/mywund_layout/mywund_mobile_05.png';
+
+import Billable01 from '../../assets/img/billable_layout/billa_desktop_01.png';
+import Billable02 from '../../assets/img/billable_layout/billa_desktop_02.png';
+import Billable03 from '../../assets/img/billable_layout/billa_desktop_03.png';
+import Billable04 from '../../assets/img/billable_layout/billa_mobile_01.png';
+import Billable05 from '../../assets/img/billable_layout/billa_mobile_02.png';
+import Billable06 from '../../assets/img/billable_layout/billa_mobile_03.png';
+import Billable07 from '../../assets/img/billable_layout/billa_mobile_04.png';
+import Billable08 from '../../assets/img/billable_layout/billa_mobile_05.png';
+
+import Ley01 from '../../assets/img/ley_layout/ley_desktop_01.png';
+import Ley02 from '../../assets/img/ley_layout/ley_desktop_02.png';
+import Ley03 from '../../assets/img/ley_layout/ley_desktop_03.png';
+
+import Prp01 from '../../assets/img/prp_layout/prp_desktop_01.png';
+import Prp02 from '../../assets/img/prp_layout/prp_desktop_02.png';
+import Prp03 from '../../assets/img/prp_layout/prp_desktop_03.png';
+import Prp04 from '../../assets/img/prp_layout/prp_desktop_04.png';
+import Prp05 from '../../assets/img/prp_layout/prp_mobile_01.png';
+import Prp06 from '../../assets/img/prp_layout/prp_mobile_02.png';
+import Prp07 from '../../assets/img/prp_layout/prp_mobile_03.png';
+import Prp08 from '../../assets/img/prp_layout/prp_mobile_04.png';
+import Prp09 from '../../assets/img/prp_layout/prp_mobile_05.png';
+
+import Sisefar01 from '../../assets/img/sisefar_layout/sisefar_desktop_01.png';
+import Sisefar02 from '../../assets/img/sisefar_layout/sisefar_desktop_02.png';
+import Sisefar03 from '../../assets/img/sisefar_layout/sisefar_desktop_03.png';
 
 export interface HomeContextType {
 	currentProjectIndex: number;
 	setCurrentProject: (index: number) => void;
 	currentTitle: string;
 	currentColorOrb: string;
+	currentInformationLabel: string;
+	onClickImage: () => void;
+	currentImage: string | undefined;
 }
 
 export function HomeProvider({ children }: { children: React.ReactNode }) {
 	const [currentProjectIndex, setCurrentProject] = useState<number>(0);
+	const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
 
 	const titles = [
 		'Projects\nPortfolio',
 		'MyWund',
-		'BillableHours',
+		'Billable\nHours',
 		'Ley\nUrgencias',
-		'Project\nRequisition Process',
+		'Personnel\nRequisition\nProcess',
 		'Landing\nSisefar',
 		'Landing\nCEI',
 		'Nurasign',
 		'Promptxr',
+	];
+
+	const informationLabels = [
+		'',
+		'Software para registrar y visualizar la evolución de heridas de pacientes\nColaboré con un equipo de enfermeras de Chile. Desarrollé el Frontend con Flutter y Backend con Flask.',
+		'Pequeño software para registrar las horas invertidas de un colaborador en un proyecto específico.\nTrabajé principalmente en el Backend con NodeJS e hice algunas funcionalidades del Frontend.',
+		'Formulario para regisrtar la estancia hospitalaria de un paciente\nMe encargué del diseño del sistema y el desarrollo del Frontend y del Backend.',
+		'Proyecto para gestionar requisiciones de personal haciendo uso de aprobaciones escalonadas.\nTrabajé en el diseño de la plataforma, en levantar los recursos y el desarrollo del Frontend, de Web y Mobile y del Backend.',
+		'Una Landing Page para el sistema SISEFARPRO, un software para la gestión y seguimiento de medicamentos.\nMe encargué del desarrollo del Frontend.',
+		'Una Landing Page para la compañía CHRISTUS CEI.\nMe encargué del desarrollo de toda la página así como de toda la funcionalidad.',
+		'Proyecto refactorizado desde cero, enfocado a la asignación de enfermeras en centros hospitalarios.\nMe encargué del diseño del Frontend, así como la limpieza y optimización de la base de datos.',
+		'Proyecto refactorizado completamente, enfocado a la realización y validación de protocolos a pacientes.\nMe encargué principalmente en el desarrollo del Frontend y en levantar recursos.',
 	];
 
 	const colorMap: Record<number, string> = {
@@ -37,11 +87,59 @@ export function HomeProvider({ children }: { children: React.ReactNode }) {
 
 	const currentColorOrb = colorMap[currentProjectIndex] || '';
 
+	const myWundImages = [MyWund01, MyWund02, MyWund03, MyWund04, MyWund05];
+
+	const billableImages = [
+		Billable01,
+		Billable02,
+		Billable03,
+		Billable04,
+		Billable05,
+		Billable06,
+		Billable07,
+		Billable08,
+	];
+
+	const leyImages = [Ley01, Ley02, Ley03];
+
+	const prpImages = [Prp01, Prp02, Prp03, Prp04, Prp05, Prp06, Prp07, Prp08, Prp09];
+
+	const sisefarImages = [Sisefar01, Sisefar02, Sisefar03];
+
+	const images: Record<number, string[]> = {
+		0: [],
+		1: myWundImages,
+		2: billableImages,
+		3: leyImages,
+		4: prpImages,
+		5: sisefarImages,
+	};
+
+	const onClickImage = () => {
+		console.log(currentImageIndex);
+		const maxIndex = images[currentProjectIndex]?.length || 0;
+		console.log(maxIndex);
+		setCurrentImageIndex((prev) => (prev + 1) % maxIndex);
+	};
+
+	const currentImage = () => {
+		return images[currentProjectIndex].length !== 0
+			? images[currentProjectIndex][currentImageIndex]
+			: undefined;
+	};
+
+	useEffect(() => {
+		setCurrentImageIndex(0);
+	}, [currentProjectIndex]);
+
 	const contextValue = {
 		currentProjectIndex,
 		setCurrentProject,
 		currentTitle: titles[currentProjectIndex],
 		currentColorOrb,
+		currentInformationLabel: informationLabels[currentProjectIndex],
+		onClickImage,
+		currentImage: currentImage(),
 	};
 
 	return <HomeContext.Provider value={contextValue}>{children}</HomeContext.Provider>;
