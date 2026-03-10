9b9a15c6996b93808ee49e57976a410e2763737a
ErikJsGlz <erikjs.glz@gmail.com>
2026-03-10 16:00:00 -0600

    Fix: Delete unnecessary code

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,24 +1,11 @@
-import { Box, Button, Stack, Typography, useMediaQuery } from '@mui/material';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useMediaQuery } from '@mui/material';
 
-import { useHomeContext } from '../../Pages/Provider/useHomeContext';
 import './Header.scss';
 import HeaderDesktop from './HeaderDesktop';
 import HeaderMobile from './HeaderMobile';
 
 export default function Header() {
-	const currentYear: string = new Date().getFullYear().toString();
-
-	const [opened, setOpened] = useState<boolean>(false);
-
-	const onToggle = () => {
-		setOpened((prev) => !prev);
-	};
-
-	const { setCurrentProject } = useHomeContext();
-
-	const isMobile = useMediaQuery('(max-width: 758px)');
+	const isMobile = useMediaQuery('(max-width: 1280px)');
 
 	return <>{isMobile ? <HeaderMobile /> : <HeaderDesktop />}</>;
 }
